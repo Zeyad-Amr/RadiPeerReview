7c096dcc3c25b806405de0b4fffc1330ed3db296
Mo'men Mohamed <84360276+momen882001@users.noreply.github.com>
2024-06-20 02:08:20 +0300

    feat : apply report details section

--- client/src/modules/request/view/pages/RequestPage.tsx
+++ client/src/modules/request/view/pages/RequestPage.tsx
@@ -1,73 +1,89 @@
-import { Feedback } from "@mui/icons-material";
-import { Box, Container } from "@mui/system";
-import React from "react";
-import MainReport from "../components/DisplayedRequest";
-import Review from "../components/DisplayedReview";
+import { Box } from "@mui/system";
+import React, { useState } from "react";
 import DisplayedRequest from "../components/DisplayedRequest";
 import DisplayedReview from "../components/DisplayedReview";
-import FeedbackComponent from "../components/FeedbackComponent";
+import { Grid } from "@mui/material";
+import ReportDetailsSection from "../components/ReportDetailsSection";
 
-interface Author {
-  name: string;
-  avatar: string;
+interface Review {
+  id: number;
+  content: string;
+  time: string;
 }
 
 interface Report {
-  author: Author;
-  content: string;
+  id: number;
+  name: string;
   time: string;
-  likes: number;
+  reviews: Review[];
 }
 
-interface Review {
-  author: Author;
-  content: string;
-  time: string;
-}
+const reports: Report[] = [
+  {
+    id: 1,
+    name: "Report 1",
+    time: "10:00 AM",
+    reviews: [{ id: 2, content: "Review 1.2", time: "10:10 AM" }],
+  },
+  {
+    id: 2,
+    name: "Report 2",
+    time: "11:00 AM",
+    reviews: [{ id: 1, content: "Review 2.1", time: "11:05 AM" }],
+  },
+];
 
 const RequestPage = () => {
-  const report: Report = {
-    author: {
-      name: "Stacey McDonald King",
-      avatar: "path/to/avatar.jpg",
-    },
-    content: "Is this a 1 bedroom apartment or shared living space?",
-    time: "1d",
-    likes: 5,
-  };
-
-  const review: Review = {
-    author: {
-      name: "Amber Taylor",
-      avatar: "path/to/avatar.jpg",
-    },
-    content:
-      "yeah it's a single bedroom apartments and it's private not shared",
-    time: "18h",
-  };
-
-  const handleFeedbackSubmit = (feedback: string) => {
-    console.log("Feedback submitted:", feedback);
-  };
+  const [reportData, setReportData] = useState<any>();
   return (
-    <Box
-      sx={{
-        margin: "1rem",
-      }}
-    >
-      <DisplayedRequest
-        author={report.author}
-        content={report.content}
-        time={report.time}
-        likes={report.likes}
-      />
-      <DisplayedReview
-        author={review.author}
-        content={review.content}
-        time={review.time}
-      />
-      <FeedbackComponent onSubmit={handleFeedbackSubmit} />
-    </Box>
+    <Grid container>
+      <Grid item lg={5} md={5} sm={12} xs={12}>
+        <Box sx={{ margin: "1rem", position: "relative" }}>
+          <Box
+            sx={{
+              position: "absolute",
+              left: "10px",
+              top: 0,
+              bottom: 0,
+              width: "2px",
+              backgroundColor: "gray",
+            }}
+          />
+          {reports.map((report, index) => (
+            <Box
+              key={report.id}
+              sx={{ position: "relative", marginLeft: "2rem" }}
+            >
+              <DisplayedRequest setReportData={setReportData} report={report} />
+              {report.reviews.map((review) => (
+                <DisplayedReview
+                  key={review.id}
+                  content={review.content}
+                  time={review.time}
+                />
+              ))}
+            </Box>
+          ))}
+        </Box>
+      </Grid>
+      <Grid
+        item
+        lg={7}
+        md={7}
+        sm={12}
+        xs={12}
+        sx={{
+          height: "100vh",
+          backgroundColor: "#fff",
+          padding: "1rem",
+          borderLeftWidth: "3px",
+          borderLeftColor: "primary.lighter",
+          borderLeftStyle: "solid",
+        }}
+      >
+        <ReportDetailsSection reportData={reportData} />
+      </Grid>
+    </Grid>
   );
 };
 
