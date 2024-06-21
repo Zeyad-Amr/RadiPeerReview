9749d10da67fe852414e146f0cd9867613d510bf
Zeyad Amr <68791488+Zeyad-Amr@users.noreply.github.com>
2024-06-21 03:12:47 +0300

    [Client] feat: Radiologist Navbar logout

--- client/src/core/layout/Notifications.tsx
+++ client/src/core/layout/Notifications.tsx
@@ -1,110 +1,126 @@
-import { Typography } from '@mui/material'
-import { Box } from '@mui/system'
-import React, { useEffect, useRef, useState } from 'react'
-import AddRoundedIcon from '@mui/icons-material/AddRounded';
-import NotificationCard from './NotificationCard';
+import { Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import React, { useEffect, useRef, useState } from "react";
+import AddRoundedIcon from "@mui/icons-material/AddRounded";
+import NotificationCard from "./NotificationCard";
 const Notifications = ({ scaleProp }: { scaleProp: boolean }) => {
-    interface notification {
-        type: string
-        body: string
-        time: string
+  interface notification {
+    type: string;
+    body: string;
+    time: string;
+  }
+  const notifications = [
+    {
+      type: "Add Report",
+      body: "Dr. Body Yasser added a new report",
+      time: "3:54 am",
+    },
+    {
+      type: "Accept Report",
+      body: "Dr. Body Yasser accepted Dr. Zeyad Amr's report",
+      time: "3:54 am",
+    },
+    {
+      type: "Reviewed Report",
+      body: "Dr. Body Yasser reviewed Dr. Zeyad Amr's report",
+      time: "3:54 am",
+    },
+    {
+      type: "Add Report",
+      body: "Dr. Body Yasser added a new report",
+      time: "3:54 am",
+    },
+    {
+      type: "Accept Report",
+      body: "Dr. Body Yasser accepted Dr. Zeyad Amr's report",
+      time: "3:54 am",
+    },
+    {
+      type: "Reviewed Report",
+      body: "Dr. Body Yasser reviewed Dr. Zeyad Amr's report",
+      time: "3:54 am",
+    },
+    {
+      type: "Accept Report",
+      body: "Dr. Body Yasser accepted Dr. Zeyad Amr's report",
+      time: "3:54 am",
+    },
+    {
+      type: "Reviewed Report",
+      body: "Dr. Body Yasser reviewed Dr. Zeyad Amr's report",
+      time: "3:54 am",
+    },
+  ];
+  const [scale, setScale] = useState(0);
+  const [firstRender, setFirstRender] = useState(true);
+  useEffect(() => {
+    if (!firstRender) {
+      setScale(1);
+    } else {
+      setFirstRender(false);
     }
-    const notifications = [
-        {
-            type: 'Add Report',
-            body: "Dr. Body Yasser added a new report",
-            time: '3:54 am',
-        },
-        {
-            type: 'Accept Report',
-            body: "Dr. Body Yasser accepted Dr. Zeyad Amr's report",
-            time: '3:54 am',
-        },
-        {
-            type: 'Reviewed Report',
-            body: "Dr. Body Yasser reviewed Dr. Zeyad Amr's report",
-            time: '3:54 am',
-        },
-        {
-            type: 'Add Report',
-            body: "Dr. Body Yasser added a new report",
-            time: '3:54 am',
-        },
-        {
-            type: 'Accept Report',
-            body: "Dr. Body Yasser accepted Dr. Zeyad Amr's report",
-            time: '3:54 am',
-        },
-        {
-            type: 'Reviewed Report',
-            body: "Dr. Body Yasser reviewed Dr. Zeyad Amr's report",
-            time: '3:54 am',
-        },
-        {
-            type: 'Accept Report',
-            body: "Dr. Body Yasser accepted Dr. Zeyad Amr's report",
-            time: '3:54 am',
-        },
-        {
-            type: 'Reviewed Report',
-            body: "Dr. Body Yasser reviewed Dr. Zeyad Amr's report",
-            time: '3:54 am',
-
-        },
-    ]
-    const [scale, setScale] = useState(0)
-    const [firstRender, setFirstRender] = useState(true)
-    useEffect(() => {
-        if (!firstRender) {
-
-            setScale(1)
-
-        } else {
-            setFirstRender(false)
-        }
-    }, [scaleProp])
-    return (
-        <><Box sx={{
-            position: 'fixed',
-            top: 0,
-            left: 0,
-            width: '100vw',
-            height: '100vh',
-            backgroundColor: '#00000000',
-            zIndex: 1,
-            display: scale === 0 ? 'none' : 'block',
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [scaleProp]);
+  return (
+    <>
+      <Box
+        sx={{
+          position: "fixed",
+          top: 0,
+          left: 0,
+          width: "100vw",
+          height: "100vh",
+          backgroundColor: "#00000000",
+          zIndex: 1,
+          display: scale === 0 ? "none" : "block",
         }}
-            onClick={() => setScale(0)}
-        />
-            <Box
-                sx={{
-                    width: '20rem', height: '30rem', backgroundColor: 'white', position: 'absolute',
-                    right: '2rem', top: '4rem',
-                    boxShadow: '0 0 20px #00000020',
-                    borderRadius: '1rem',
-                    padding: '1rem',
-                    boxSizing: 'border-box',
-                    overflowY: 'auto',
-                    transition: '0.2s',
-                    scale: (scale),
-                    transformOrigin: 'top right',
-                    zIndex: 2
+        onClick={() => setScale(0)}
+      />
+      <Box
+        sx={{
+          width: "20rem",
+          height: "30rem",
+          backgroundColor: "white",
+          position: "absolute",
+          right: "2rem",
+          top: "4rem",
+          boxShadow: "0 0 20px #00000020",
+          borderRadius: "1rem",
+          padding: "1rem",
+          boxSizing: "border-box",
+          overflowY: "auto",
+          transition: "0.2s",
+          scale: scale,
+          transformOrigin: "top right",
+          zIndex: 2,
+        }}
+      >
+        <Typography sx={{ color: "primary.light" }}>Notifications</Typography>
+        <Box
+          sx={{
+            padding: "1rem 0",
+            boxSizing: "border-box",
+          }}
+        >
+          {notifications.map((notification: notification, i: number) => (
+            <>
+              <NotificationCard key={i} notification={notification} />
+              {i !== notifications.length - 1 ? (
+                <Box
+                  sx={{
+                    width: "100%",
+                    height: "2px",
+                    backgroundColor: "primary.lighter",
+                    margin: "0.75rem 0",
+                  }}
+                />
+              ) : null}
+            </>
+          ))}
+        </Box>
+      </Box>
+    </>
+  );
+};
 
-                }}>
-                <Typography sx={{ color: 'primary.light' }}>Notifications</Typography>
-                <Box sx={{
-                    padding: '1rem 0',
-                    boxSizing: 'border-box',
-                }}>
-                    {notifications.map((notification: notification, i: number) => (
-                        <>
-                            <NotificationCard key={i} notification={notification} />
-                            {i !== notifications.length - 1 ? <Box sx={{ width: '100%', height: '2px', backgroundColor: 'primary.lighter', margin: '0.75rem 0' }} /> : null}
-                        </>
-                    ))}
-                </Box>
-            </Box></>
-    )
-}
-
-export default Notifications
+export default Notifications;
--- client/src/core/layout/radiologist/RadiologistNavbar.tsx
+++ client/src/core/layout/radiologist/RadiologistNavbar.tsx
@@ -5,172 +5,133 @@ import Toolbar from "@mui/material/Toolbar";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
 import Badge from "@mui/material/Badge";
-
-import MenuIcon from "@mui/icons-material/Menu";
-// import AccountCircle from "@mui/icons-material/AccountCircle";
-// import MailIcon from "@mui/icons-material/Mail";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import NotificationsIcon from "@mui/icons-material/Notifications";
 import { useRouter } from "next/navigation";
-import Notifications from "..//Notifications";
+import Notifications from "../Notifications";
+import { useAppDispatch } from "@/core/state/store";
+import { logout } from "@/modules/auth/controllers/thunks/auth-thunk";
 
 export default function RadiologistNavbar() {
-    React.useState<null | HTMLElement>(null);
-    const [scale, setScale] = React.useState(false)
+  const [scale, setScale] = React.useState(false);
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const [currentDateTime, setCurrentDateTime] = React.useState<Date>(
+    new Date()
+  );
 
-    const router = useRouter();
-    const [currentDateTime, setCurrentDateTime] = React.useState<Date>(
-        new Date()
-    );
+  const router = useRouter();
+  const dispatch = useAppDispatch();
 
-    React.useEffect(() => {
-        const intervalId = setInterval(() => {
-            setCurrentDateTime(new Date());
-        }, 1000);
+  React.useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentDateTime(new Date());
+    }, 1000);
 
-        // Cleanup the interval on component unmount
-        return () => clearInterval(intervalId);
-    }, []); 
+    // Cleanup the interval on component unmount
+    return () => clearInterval(intervalId);
+  }, []);
 
-    const formattedDate = currentDateTime.toISOString().split("T")[0];
-    const formattedTime = currentDateTime.toTimeString().split(" ")[0];
+  const formattedDate = currentDateTime.toISOString().split("T")[0];
+  const formattedTime = currentDateTime.toTimeString().split(" ")[0];
 
-    return (
-        <Box sx={{ flexGrow: 1 }}>
-            <AppBar
-                position="static"
-                sx={{ backgroundColor: "primary.lighter", boxShadow: "none" }}
+  const handleMenuOpen = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleLogout = () => {
+    // Add logout logic here
+    dispatch(logout());
+    router.push("/login");
+  };
+
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar
+        position="static"
+        sx={{ backgroundColor: "primary.lighter", boxShadow: "none" }}
+      >
+        <Toolbar
+          sx={{
+            justifyContent: "space-between",
+            alignItems: "center",
+            padding: "1rem 3rem",
+          }}
+        >
+          <Box sx={{ display: "flex", flexDirection: "column" }}>
+            <Typography sx={{ fontSize: "0.8rem", color: "primary.light" }}>
+              Hello
+            </Typography>
+            <Typography
+              sx={{
+                fontSize: "1rem",
+                color: "primary.main",
+                fontWeight: "bold",
+              }}
             >
-                <Toolbar
-                    sx={{
-                        justifyContent: "space-between",
-                        padding: {
-                            lg: "1rem 3rem",
-                            md: "1rem 3rem",
-                            sm: "1rem 1rem",
-                            xs: "1rem 1rem",
-                        },
-                    }}
-                >
-                    <Box
-                        sx={{
-                            display: { xs: "none", md: "flex" },
-                            justifyContent: "center",
-                            alignItems: "center",
-                        }}
-                    >
-                        <Box>
-                            <Typography sx={{ fontSize: '0.8rem', color: 'primary.light' }}> Hello </Typography>
-                            <Typography sx={{ fontSize: '1rem', color: 'primary.main', fontWeight: 'bold' }}> Dr. Body Yaser </Typography>
-                        </Box>
-                        <Box onClick={() => router.push('/')}>
-                            <Box
-                                component="img"
-                                src=".././assets/hlogo.png "
-                                alt=""
-                                sx={{
-                                    width: "12rem",
-                                    display: { xs: "none", md: "flex" },
-                                    mr: 1,
-                                }}
-                            ></Box>
-                        </Box>
-                    </Box>
-                    {/* <Search>
-            <SearchIconWrapper>
-              <SearchIcon />
-            </SearchIconWrapper>
-            <StyledInputBase
-              placeholder="Search…"
-              inputProps={{ "aria-label": "search" }}
-            />
-          </Search> */}
-                    <Box
-                        sx={{
-                            display: { xs: "none", md: "flex" },
-                            flexDirection: "column",
-                            justifyContent: "center",
-                            alignItems: "center",
-                            position: "absolute",
-                            left: "50%",
-                            transform: "translateX(-50%)",
-                        }}
-                    >
-                        <Typography sx={{ fontWeight: "600" }}>
-                            {formattedDate}
-                        </Typography>
-                        <Typography >
-                            {formattedTime}
-                        </Typography>
-                    </Box>
-                    <Box sx={{ display: { xs: "none", md: "flex" } }}>
-                        <IconButton
-                            size="large"
-                            aria-label="show 17 new notifications"
-                            color="inherit"
-                            onClick={() => setScale(!scale)}
-                        >
-                            <Badge badgeContent={17} sx={{
-                                "& .MuiBadge-badge": {
-                                    backgroundColor: "secondary.main",
-                                    color: 'white'
-                                }
-                            }}>
-                                <NotificationsIcon sx={{ color: 'primary.main' }} />
-                            </Badge>
-                        </IconButton>
-                    </Box>
-                    <Notifications scaleProp={scale} />
-                    {/* ###################################################################### */}
-                    {/* ############################### MOBILE ############################### */}
-                    {/* ###################################################################### */}
-
-                    <Box
-                        sx={{
-                            display: { xs: "flex", md: "none" },
-                            width: "100%",
-                            justifyContent: "space-between",
-                        }}
-                    >
-                        <Box sx={{ display: "flex" }}>
-                            <Box>
-                                <Typography sx={{ fontSize: '0.8rem', color: 'primary.light' }}> Hello </Typography>
-                                <Typography sx={{ fontSize: '1rem', color: 'primary.main', fontWeight: 'bold' }}> Dr. Body Yaser </Typography>
-                            </Box>
-                            <Box onClick={() => router.push('/')}>
-                                <Box
-                                    component="img"
-                                    src=".././assets/hlogo.png "
-                                    alt=""
-                                    sx={{
-                                        width: "7rem",
-                                        display: { lg: "none", md: "none", sm: "flex", xs: "flex" },
-                                        mr: 1,
-                                    }}
-                                ></Box>
-                            </Box>
-                        </Box>
-                        <Box sx={{ display: { xs: "flex", md: "none" } }}>
-
-                            <IconButton
-                                size="large"
-                                aria-label="show 17 new notifications"
-                                color="inherit"
-                                onClick={() => setScale(!scale)}
-                            >
-                                <Badge badgeContent={17} sx={{
-                                    "& .MuiBadge-badge": {
-                                        backgroundColor: "secondary.main",
-                                        color: 'white'
-                                    }
-                                }}>
-                                    <NotificationsIcon sx={{ color: 'primary.main' }} />
-                                </Badge>
-                            </IconButton>
-
-                        </Box>
-                    </Box>
-                </Toolbar>
-            </AppBar>
-        </Box>
-    );
+              Dr. Body Yaser
+            </Typography>
+          </Box>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <Typography>{formattedDate}</Typography>
+            <Typography>{formattedTime}</Typography>
+          </Box>
+          <Box>
+            <IconButton
+              size="large"
+              aria-label="show 17 new notifications"
+              color="inherit"
+              onClick={() => setScale(!scale)}
+            >
+              <Badge
+                badgeContent={17}
+                sx={{
+                  "& .MuiBadge-badge": {
+                    backgroundColor: "secondary.main",
+                    color: "white",
+                  },
+                }}
+              >
+                <NotificationsIcon sx={{ color: "primary.main" }} />
+              </Badge>
+            </IconButton>
+            <IconButton
+              size="large"
+              aria-label="show account menu"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleMenuOpen}
+            >
+              <AccountCircleIcon sx={{ color: "primary.main" }} />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorEl}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "left",
+              }}
+              keepMounted
+              open={Boolean(anchorEl)}
+              onClose={handleMenuClose}
+            >
+              <MenuItem onClick={handleLogout}>Logout</MenuItem>
+            </Menu>
+          </Box>
+          <Notifications scaleProp={scale} />
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
 }
