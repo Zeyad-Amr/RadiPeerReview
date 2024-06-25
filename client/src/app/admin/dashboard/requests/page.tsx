b8fcf042edc722d1d81ad36e364668e2c2aba1e8
Zeyad Amr <68791488+Zeyad-Amr@users.noreply.github.com>
2024-06-25 21:57:17 +0300

    [Client] refactor: Enhance Admin Requests Table UI

--- client/src/app/admin/dashboard/requests/page.tsx
+++ client/src/app/admin/dashboard/requests/page.tsx
@@ -2,7 +2,7 @@
 
 import Layout from "@/core/layout/Layout";
 import ProtectedLayout from "@/core/shared/components/ProtectedLayout";
-import ReportsPage from "@/modules/review-requests/view/pages/admin/ReportsPage";
+import AdminRequestsPage from "@/modules/review-requests/view/pages/admin/AdminRequestsPage";
 import React from "react";
 import { Role } from "@/core/shared/constants/enums";
 
@@ -10,7 +10,7 @@ const page = () => {
   return (
     <ProtectedLayout allowedRoles={[Role.ADMIN]}>
       <Layout>
-        <ReportsPage />
+        <AdminRequestsPage />
       </Layout>
     </ProtectedLayout>
   );
---
+++ client/src/modules/review-requests/view/pages/radiologist/RadiologistRequestsPage.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Halfs from "../../components/home/Halfs";
+
+const RadiologistRequestsPage = () => {
+  return <Halfs />;
+};
+
+export default RadiologistRequestsPage;
